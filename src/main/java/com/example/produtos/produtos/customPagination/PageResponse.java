2acfb6c8e68209c66b72d574c2984dd672e552c9
Julio Cesar <jccintr@gmail.com>
2025-05-16 21:20:31 -0300

    Alterado PageResponse de class para record

--- src/main/java/com/example/produtos/produtos/customPagination/PageResponse.java
+++ src/main/java/com/example/produtos/produtos/customPagination/PageResponse.java
@@ -4,82 +4,30 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 
-public class PageResponse<T> {
+public record PageResponse<T>(
+		List<T> content,
+	    int pageNumber,
+	    int pageSize,
+	    long totalElements,
+	    int totalPages,
+	    boolean first,
+	    boolean last
+		)
+{
 	
-	private List<T> content;
-    private int pageNumber;
-    private int pageSize;
-    private long totalElements;
-    private int totalPages;
-    private boolean first;
-    private boolean last;
+	
     
     public PageResponse(Page<T> page) {
     	
-    	this.content = page.getContent();
-    	this.pageNumber = page.getNumber();
-    	this.pageSize = page.getSize();
-    	this.totalElements = page.getTotalElements();
-    	this.totalPages = page.getTotalPages();
-    	this.first = page.isFirst();
-    	this.last = page.isLast();
+       this(page.getContent(),
+	    	page.getNumber(),
+	    	page.getSize(),
+	    	page.getTotalElements(),
+	    	page.getTotalPages(),
+	    	page.isFirst(),
+	    	page.isLast() );
     	
     }
 
-	public List<T> getContent() {
-		return content;
-	}
-
-	public void setContent(List<T> content) {
-		this.content = content;
-	}
-
-	public int getPageNumber() {
-		return pageNumber;
-	}
-
-	public void setPageNumber(int pageNumber) {
-		this.pageNumber = pageNumber;
-	}
-
-	public int getPageSize() {
-		return pageSize;
-	}
-
-	public void setPageSize(int pageSize) {
-		this.pageSize = pageSize;
-	}
-
-	public long getTotalElements() {
-		return totalElements;
-	}
-
-	public void setTotalElements(long totalElements) {
-		this.totalElements = totalElements;
-	}
-
-	public int getTotalPages() {
-		return totalPages;
-	}
-
-	public void setTotalPages(int totalPages) {
-		this.totalPages = totalPages;
-	}
-
-	public boolean isFirst() {
-		return first;
-	}
-
-	public void setFirst(boolean first) {
-		this.first = first;
-	}
-
-	public boolean isLast() {
-		return last;
-	}
-
-	public void setLast(boolean last) {
-		this.last = last;
-	}
     
 }
--- src/main/java/com/example/produtos/produtos/services/ProductService.java
+++ src/main/java/com/example/produtos/produtos/services/ProductService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.example.produtos.produtos.customPagination.PageResponse;
@@ -20,7 +21,7 @@ public class ProductService {
 	
 	public PageResponse<ProductDTO> findAll(Pageable pageable){
 		
-		Pageable page = PageRequest.of(pageable.getPageNumber(), Math.min(pageable.getPageSize(),100));
+		Pageable page = PageRequest.of(pageable.getPageNumber(), Math.min(pageable.getPageSize(),100),Sort.by("name"));
 		Page<Product> products = repository.findAll(page);
 		Page<ProductDTO> productDTOPage = products.map(ProductDTO::new);
 		return new PageResponse<>(productDTOPage);
